56839d0c54efdb3700261f34282d206e5277c942
Shady Arafeh <shady@simplify9.com>
2018-12-10 16:03:19 +0200

    new changes

--- SW.Searchy.UnitTests/UnitTest1.cs
+++ SW.Searchy.UnitTests/UnitTest1.cs
@@ -41,9 +41,9 @@ namespace SW.Searchy.UnitTests
             SearchQuery  _sq= new SearchQuery() ;
             SearchCondition _sc = new SearchCondition( new FilterByOptions { FilterFor= "Files", FilterOperator=FilterByOptions.FilterOperatorOptions.Contains, MemberName= "Name" });
             _sq.Conditions.Add(_sc);  
-            var _data =_context.UseDms().Repositories.Search(_sq);
+            var _data =_context.UseDms().Repositories.Search(_sq).ToList();
 
-            var _count = _data.Count();  
+            Assert.AreEqual(7, _data.Count);
         }
 
         [TestMethod]
@@ -55,6 +55,9 @@ namespace SW.Searchy.UnitTests
             List<OrderByOptions> _ob = new List<OrderByOptions> { new OrderByOptions("Name", OrderByOptions.Order.ASC) };
 
             var _data = _context.UseDms().Repositories.Search(_sq, _ob, 0, 0).ToList();
+            Assert.AreEqual(7, _data.Count);
+
+            
 
         }
 
@@ -67,12 +70,13 @@ namespace SW.Searchy.UnitTests
             List<OrderByOptions> _ob = new List<OrderByOptions> { new OrderByOptions("Name", OrderByOptions.Order.ASC) };
 
             var _data0 = _context.UseDms().Repositories.Search(_sq, _ob, 2, 0).ToList();
+            Assert.AreEqual(2, _data0.Count);
 
             var _data1 = _context.UseDms().Repositories.Search(_sq, _ob,2, 1).ToList();
-
+            Assert.AreEqual(2, _data1.Count);
 
             var _data2 = _context.UseDms().Repositories.Search(_sq, _ob, 3, 2).ToList();
-
+            Assert.AreEqual(1, _data2.Count);
         }
 
         [TestMethod]
@@ -87,7 +91,7 @@ namespace SW.Searchy.UnitTests
             _sq.Conditions.Add(_sc);
 
             var _data = _context.UseDms().Repositories.Search(_sq).ToList();
-
+            Assert.AreEqual(2, _data.Count);
 
         }
 
@@ -100,7 +104,7 @@ namespace SW.Searchy.UnitTests
             SearchCondition _sc2 = new SearchCondition(new FilterByOptions { FilterFor = 2000, FilterOperator = FilterByOptions.FilterOperatorOptions.EqualsTo, MemberName = "MaxDocSize" });
 
             var _data = _context.UseDms().Repositories.Search(_sq).ToList();
-
+            Assert.AreEqual(7, _data.Count);
         }
 
         public void Dispose()
